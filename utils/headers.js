5fbf2e1914fbadc8e5d7a4ea46831b6fb0621d73
ming wu <a85062@pchome.com.tw>
2022-05-19 04:11:31 +0800

    【新增】CORS標頭

--- utils/headers.js
+++ utils/headers.js
@@ -1,13 +1,10 @@
 const headers = (res) => {
-  const headers = {
-    'Access-Control-Allow-Headers':
-      'Content-Type, Authorization, Content-Length, X-Requested-With',
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
-    'Content-Type': 'application/json',
-  };
-
-  return res.writeHeader(200, headers);
+  res.set('Access-Control-Allow-Headers',
+  'Content-Type, Authorization, Content-Length, X-Requested-With')
+  res.set('Access-Control-Allow-Origin', '*')
+  res.set('Access-Control-Allow-Methods', 'PATCH, POST, GET,OPTIONS,DELETE')
+  res.set('Content-Type', 'application/json')
+  return res;
 };
 
 module.exports = {headers}
